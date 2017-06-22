5f8af0e3434e08ab76046d32ad79d41d2c0dfbf8
Francisco Palos <francisco.palos@gmail.com>
2017-06-22 23:22:44 +0200

    Create initial project and add first example. It have been copied from http://vertx.io/blog/my-first-vert-x-3-application/
    Signed-off-by: Francisco Palos

---
+++ vertx.introduction/src/main/java/com/fmpb/vertx/introduction/MyFirstVerticle.java
@@ -0,0 +1,27 @@
+package com.fmpb.vertx.introduction;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyFirstVerticle extends AbstractVerticle
+{
+   
+	@Override
+	public void start(Future<Void> future){
+		vertx.createHttpServer().requestHandler(r ->{r.response().end("<h1>Hello from my first " +
+	              "Vert.x 3 application</h1>");})
+		.listen(8080,result ->{
+			if(result.succeeded()){
+				future.complete();
+			}else{
+				future.fail(result.cause());
+			}
+		});
+		
+	}
+	
+}
